3f614a4f322ff08e137d4fb60e34d01e827d2734
Florian Frank <flori@ping.de>
2012-02-20 19:08:18 +0100

    Set some debugging / optimisation opts in compiler

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,21 @@
 require 'mkmf'
 require 'rbconfig'
-if CONFIG['CC'] == 'gcc'
-  CONFIG['CC'] = 'gcc -Wall '
+
+if CONFIG['CC'] =~ /gcc/
+  $CFLAGS << ' -Wall'
+  if ENV['DEBUG']
+    $CFLAGS << ' -O0 -ggdb'
+  else
+    $CFLAGS << ' -O3'
+  end
+else
+  $CFLAGS << ' -O3'
 end
 create_makefile 'amatch_ext' 
+
+# WTF, is this really the only way to get a decent output out of mkmf?
+mf = File.read 'Makefile'
+if mf.sub!(/V = 0/, 'V = 1')
+  warn "Rewriting the generated Makefile to get my compiler output displayed..."
+  File.open('Makefile', 'w') { |f| f.write mf }
+end
